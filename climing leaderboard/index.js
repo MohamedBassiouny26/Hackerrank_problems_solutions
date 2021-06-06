9d17d23ae83d4f5a895c683bfd904622cb147ef2
Mohamed Bassiouny <36187768+MohamedBassiouny26@users.noreply.github.com>
2021-06-06 22:12:00 +0200

    Update index.js

--- climing leaderboard/index.js
+++ climing leaderboard/index.js
@@ -40,52 +40,15 @@ function climbingLeaderboard(ranked, player) {
     })
     let scoreIndex = 0;
     let result = [];
-    let oldScores = [];
-    let rankMapKeys = Object.keys(rankedMap)
     let rankIndex = ranked.length - 1;
-    // while (scoreIndex < player.length) {
-    //     if (oldScores.indexOf(player[scoreIndex]) >= 0) {
-    //         result.push(result[result.length - 1]);
-    //         scoreIndex++;
-    //     }
-    //     if (player[scoreIndex] < parseInt(rankMapKeys[rankIndex])) {
-    //         result.push(rankedMap[ran]);
-    //         oldScores.push(player[scoreIndex]);
-    //         scoreIndex++;
-    //     }
-    //     else if (player[scoreIndex] >= parseInt(rankMapKeys[rankIndex]) && player[scoreIndex] < parseInt(rankMapKeys[rankIndex + 1])) {
-    //         result.push(rankedMap[rankMapKeys[rankIndex]]);
-    //         oldScores.push(player[scoreIndex]);
-    //         scoreIndex++;
-    //     }
-    //     else if (rankIndex == rankMapKeys.length - 1) {
-    //         if (player[scoreIndex] >= parseInt(rankMapKeys[rankIndex]))
-    //             result.push(rankedMap[rankMapKeys[rankIndex]]);
-    //         else
-    //             result.push(rankedMap[rankMapKeys[rankIndex]] + 1);
-    //         oldScores.push(player[scoreIndex]);
-    //         scoreIndex++;
-    //     }
-    //     if (scoreIndex === 10) {
-    //         console.log(rankMapKeys[rankIndex]);
-    //     }
-    //     rankIndex++;
-    // }
-    while (scoreIndex < player.length && rankIndex >= 0) {
+    while (scoreIndex < player.length  && rankIndex>=0) {
         let currentRank = ranked[rankIndex];
-        if (oldScores.indexOf(player[scoreIndex]) >= 0) {
-            result.push(result[result.length - 1]);
-            oldScores.push(player[scoreIndex]);
-            scoreIndex++;
-        }
-        else if (player[scoreIndex] < currentRank) {
+         if (player[scoreIndex] < currentRank) {
             result.push(rankedMap[currentRank] + 1)
-            oldScores.push(player[scoreIndex]);
             scoreIndex++;
         }
         else if (player[scoreIndex] >= currentRank && player[scoreIndex] < ranked[rankIndex - 1]) {
             result.push(rankedMap[currentRank])
-            oldScores.push(player[scoreIndex]);
             scoreIndex++;
         }
         else if (rankIndex === 0) {
@@ -93,7 +56,6 @@ function climbingLeaderboard(ranked, player) {
                 result.push(rankedMap[currentRank]);
             else
                 result.push(rankedMap[ranked[rankIndex - 1]])
-            oldScores.push(player[scoreIndex]);
             scoreIndex++;
         }
         else
@@ -101,6 +63,7 @@ function climbingLeaderboard(ranked, player) {
     }
     return result;
 }
+
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
